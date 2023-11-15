4d5d2007068ebdffdab14cbd4e03ade3bffad3d3
axdbertuol <axdber@gmail.com>
2023-11-15 20:35:17 -0300

    feat: enum

--- src/index.ts
+++ src/index.ts
@@ -231,4 +231,20 @@ export interface IPaginationOptions {
   limit: number;
 }
 
+export enum AuthProvidersEnum {
+  credentials = "credentials",
+  facebook = "facebook",
+  google = "google",
+  twitter = "twitter",
+  apple = "apple",
+}
+export enum RoleEnum {
+  "admin" = 1,
+  "user" = 2,
+}
+export enum StatusEnum {
+  "active" = 1,
+  "inactive" = 2,
+}
+
 export { IEntityHelper };
